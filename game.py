916e7ef642cb13ebc682ffefa08bce8b1a5cf5fc
konjpi1 <52383917+konjpi1@users.noreply.github.com>
2022-08-11 13:56:00 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,19 +1,35 @@
+import os
 import pygame
   
 # Form screen with 1200x900 size
 # and with resizable
+width = 1200
+heigth = 900
+fps = 120
 screen = pygame.display.set_mode((1200, 900), 
                                  pygame.RESIZABLE)
-  
+
+
+
+def draw():
+    
+    pygame.display.update() 
+
 # title
 pygame.display.set_caption('PAC - MAN')
-  
-# run window
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-  
+def main():
+    clock = pygame.time.Clock() #delay 
+    running = True
+    while running: #while running
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+               
+    pygame.quit()
 # quit pygame after closing window
-pygame.quit()
+
+
+
+if __name__ == "__main__":
+    main()
